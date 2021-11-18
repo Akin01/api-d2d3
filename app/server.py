92772ae9694d8655df2968b89b372dd2639d9155
Ainul Yaqin <Akinpasha82@gmail.com>
2021-11-18 19:27:28 +0800

    deploy api

---
+++ app/server.py
@@ -0,0 +1,46 @@
+import flask
+from flask import request, jsonify, abort
+import json
+from datetime import datetime
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+data = []
+
+@app.route('/api/ipk/data', methods=['GET'])
+def ipk_data():
+    return jsonify(data)
+
+@app.route('/api/ipk', methods=['GET'])
+def ipk_nama():
+
+    ipk_get_name = request.args['nama']
+
+    if not len(data):
+        return jsonify(data)
+
+    for mahasiswa in data:
+        if mahasiswa['nama'].lower() == ipk_get_name.lower():
+            return jsonify(mahasiswa)
+        elif not ipk_get_name:
+            return "Masukkan query dengan benar!!!"
+        
+    abort(404, description="IPK not found")
+
+
+@app.route('/api/ipk/baru', methods=['POST'])
+def update_record():
+    record = json.loads(request.data)
+    record['time'] = datetime.now().strftime('%H:%M:%S')
+
+    if not len(data):
+        data.append(record)
+        return jsonify(record)
+    
+    for mahasiswa in data:
+        if mahasiswa['id'] == record['id']:
+            return "id forbidden!!!"
+
+    data.append(record)
+    return jsonify(record)
