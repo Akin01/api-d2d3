56b5458aac089ce25f2e91db872a3185f6aca091
Ainul Yaqin <Akinpasha82@gmail.com>
2021-11-19 20:40:12 +0800

    changes

--- app/server.py
+++ app/server.py
@@ -5,43 +5,13 @@ from datetime import datetime
 
 app = flask.Flask(__name__)
 
-data = [
-    {
-        "id": 1,
-        "nama": "Yosa",
-        "ipk": 3.72,
-        "time": datetime.now().strftime('%H:%M:%S')
-    },
-    {
-        "id": 2,
-        "nama": "yazid",
-        "ipk": 3.61,
-        "time": datetime.now().strftime('%H:%M:%S')
-    }
-]
+data = []
 
-@app.route('/api/ipk/data', methods=['GET'])
+@app.route('/ipk/data', methods=['GET'])
 def ipk_data():
     return jsonify(data)
 
-@app.route('/api/ipk', methods=['GET'])
-def ipk_nama():
-
-    ipk_get_name = request.args['nama']
-
-    if not len(data):
-        return jsonify(data)
-
-    for mahasiswa in data:
-        if mahasiswa['nama'].lower() == ipk_get_name.lower():
-            return jsonify(mahasiswa)
-        elif not ipk_get_name:
-            return "Masukkan query dengan benar!!!"
-        
-    abort(404, description="IPK not found")
-
-
-@app.route('/api/ipk/baru', methods=['POST'])
+@app.route('/ipk/baru', methods=['POST'])
 def update_record():
     record = json.loads(request.data)
     record['time'] = datetime.now().strftime('%H:%M:%S')
@@ -49,10 +19,6 @@ def update_record():
     if not len(data):
         data.append(record)
         return jsonify(record)
-    
-    for mahasiswa in data:
-        if mahasiswa['id'] == record['id']:
-            return "id forbidden!!!"
 
     data.append(record)
     return jsonify(record)
